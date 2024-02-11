0bca817f5078450304936ec16befcef26ff648f8
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-02-11 08:52:40 +0400

    add: previous button click animation effect

--- carousel.js
+++ carousel.js
@@ -13,12 +13,28 @@ prevButton.onclick = function(){
     showSlider('prev');
 }
 
+let unAcceptClick;
+
 const showSlider = (type) => {
+    nextButton.style.pointerEvents = 'none';
+    prevButton.style.pointerEvents = 'none';
+
+    carousel.classList.remove('prev', 'next');
     let items = document.querySelectorAll('.carousel .list .item');
     if(type === 'next'){
         listHTML.appendChild(items[0]);
         carousel.classList.add('next');
+    }else{
+        let positionLast = items.length -1;
+        listHTML.prepend(items[positionLast]);
+        carousel.classList.add('prev');
     }
+    
+    clearTimeout(unAcceptClick);
+    unAcceptClick = setTimeout(() => {
+        nextButton.style.pointerEvents = 'auto';
+        prevButton.style.pointerEvents = 'auto';
+    }, 2000);
 }
 
 
