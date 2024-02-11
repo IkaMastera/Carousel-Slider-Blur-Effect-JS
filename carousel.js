46e076e7228353dc4582a5ec510a967da573c0aa
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-02-11 08:29:56 +0400

    add: setup javascript variables

---
+++ carousel.js
@@ -0,0 +1,16 @@
+let nextButton = document.getElementById('next');
+let prevButton = document.getElementById('prev');
+let backButton = document.getElementById('back');
+let seeMoreButtons = document.querySelectorAll('.seemore');
+let carousel = document.querySelector('.carousel');
+let listHTML = document.querySelector('.carousel .list');
+
+nextButton.onclick = function(){
+    showSlider('next');
+}
+
+prevButton.onclick = function(){
+    showSlider('prev');
+}
+
+
